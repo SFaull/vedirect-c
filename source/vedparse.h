0e2e6cc39b6174798843c9d8f0c15114e715e2b6
Sam Faull <s.faull@outlook.com>
2025-04-14 20:36:36 +0100

    Added C++ wrapper defines to header file and public declaration for reset function

--- source/vedparse.h
+++ source/vedparse.h
@@ -1,5 +1,12 @@
+#ifndef VEDPARSE_H
+#define VEDPARSE_H
+
 #include <stdint.h>
 #include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #define VEDPARSE_KEY_LENGTH_MAX     128
 #define VEDPARSE_VALUE_LENGTH_MAX   128
@@ -25,5 +32,12 @@ typedef struct
 } vedframe_t;
 
 void VEDPARSE_init(void);
+void VEDPARSE_reset(void);
 bool VEDPARSE_process(uint8_t);
 int32_t VEDPARSE_get_frame(vedframe_t* pFrame);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // VEDPARSE_H
